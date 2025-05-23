4c2b634ba13834126ad0a7a9cd7e56e86d2fa450
Deimos482 <samuel.godinho@a.fecaf.com.br>
2025-05-23 01:13:22 -0300

    Update teste_01.py

--- testes/teste_01.py
+++ testes/teste_01.py
@@ -1,1 +1,12 @@
+import requests
+
+def test_status_api_reqres():
+    url = "https://reqres.in/api/users/2"
+    response = requests.get(url)
+    status = response.status_code
+    assert status == 200, f"Erro: Esperado status 200, mas obteve {status}"
+    print("✅ Teste 01 passou com sucesso - Status 200 recebido")
+
+# Executar o teste
+test_status_api_reqres()
 
